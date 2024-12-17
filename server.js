e090907807400383fe4f7e63375dc2685cc30362
justinrivera382 <justinrivera382@proton.me>
2024-12-17 13:26:44 -0500

    Creating Routes & Responses

--- server.js
+++ server.js
@@ -7,6 +7,37 @@ dotenv.config({ path: "./config/config.env" });
 // initialize app
 const app = express();
 
+// basic route template creation using EXPRESS
+// app.get("/", (req, res) => {
+//   res.status(200).json({ success: true, data: { id: 1 } });
+// });
+
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Updated bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Deleted bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 // set up server
