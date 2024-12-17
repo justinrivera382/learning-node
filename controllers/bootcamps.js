3851ead451855f0e85b46ca754e541841e03ce89
justinrivera382 <justinrivera382@proton.me>
2024-12-17 14:15:38 -0500

    Creating Controller Methods

---
+++ controllers/bootcamps.js
@@ -0,0 +1,43 @@
+// here we will be creating different methods that will be associated in certain routes, this is where we will be adding all of our functionality for those on the ASSOCIATED url path of /api/v1/bootcamps/
+
+// we're going to export each method so we can bring it into the routes file
+// these are basically "middleware functions" so they will take in a (req, res, next), middleware are essentially functions that have access to the request-response cycle (req, res). therefore they run DURING the cycle. you can create authentication middleware, error handling middleware, etc... you will learn more later about middleware
+
+// @desk        Get all bootcamps
+// @route       GET /api/v1/bootcamps
+// @access      Public -ERASE:no token required:ERASE-
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
+
+// @desk        Get single bootcamp
+// @route       GET /api/v1/bootcamps/:id
+// @access      Public -ERASE:no token required:ERASE-
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+};
+
+// @desk        Create new bootcamp
+// @route       POST /api/v1/bootcamps
+// @access      Private -ERASE:token required/sign in/etc...:ERASE-
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+};
+
+// @desk        Update bootcamp
+// @route       PUT /api/v1/bootcamps/:id
+// @access      Private -ERASE:token required/sign in/etc...:ERASE-
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desk        Delete bootcamp
+// @route       Delete /api/v1/bootcamps/:id
+// @access      Private -ERASE:token required/sign in/etc...:ERASE-
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,39 +1,26 @@
 const express = require("express");
+
+// we delete the original routes and exported them from our controllers directory to clean everything up and now they're imported into our "routes/bootcamps.js"
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 // initialize router and we will no longer have access to app here and instead we will be using "router"
 // the entire point of the express.Router() is to give us the ability to "modularize" and maintain our url paths more easily
 // you should see when we go use the "user signup" route where we will be authenticating users with a different route in, probably, a different file called "authentication.js" or something like that
 const router = express.Router();
 
-// basic route template creation using EXPRESS
-// app.get("/", (req, res) => {
-//   res.status(200).json({ success: true, data: { id: 1 } });
-// });
+// here since our getBootcamps and createBootcamp found in our "./controllers/bootcamp.js" file shares the same base url path, in this case "/api/v1/bootcamps" which we got from our "./server.js" file from [app.use("/api/v1/bootcamps", bootcamps)] which gets pushed into "./controllers/bootcamps.js"
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-// since we will be using router change "app.[crud operation here]("/...")" to "router.[crud operation here]("/...")"
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
-
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Updated bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Deleted bootcamp ${req.params.id}` });
-});
+// since our getBootcamp, updateBootcamp, and deleteBootcamp require the :id, we have to route them like this and, just like stated earlier, we get the rest of the base url path, "/api/v1/bootcamps" from our "./server.js" file from [app.use("/api/v1/bootcamps", bootcamps)] which gets pushed into "./controllers/bootcamps.js"
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
