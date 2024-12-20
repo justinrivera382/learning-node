d56103c591682a4aab4fc42856eedceab7a97102
justinrivera382 <justinrivera382@proton.me>
2024-12-20 15:59:27 -0500

    Course Model & Seeding

--- seeder.js
+++ seeder.js
@@ -13,6 +13,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI);
@@ -22,11 +23,16 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 // Import into database
 const importData = async () => {
   try {
     // we don't need to respond with anything, the goal is to just import the data into the database, therefore we don't need to save it into a variable
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -39,6 +45,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log("Data Destroyed...".red.inverse);
     process.exit();
